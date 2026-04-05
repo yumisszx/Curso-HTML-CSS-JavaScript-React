b3d2b2225d24ee97e6d121a0f835c50b086ff8a1
yumi <ligiayumimeira@gmail.com>
2026-04-05 20:43:38 -0300

    add js

---
+++ hospedagem/app.js
@@ -0,0 +1,18 @@
+//elementos
+const root = document.documentElement;
+const btn = document.getElementById("btnTema");
+const ano = document.getElementById("ano");
+
+//preecher ano atual
+ano.textContent = new Date().getFullYear();
+
+//tema
+btn.addEventListener("click", () =>{
+    const escuro = root.getAttribute("data-tema") === 'escuro'
+
+    if(escuro){
+        root.removeAttribute("data-tema")
+    }else{
+        root.setAttribute("data-tema", "escuro")
+    }
+})
